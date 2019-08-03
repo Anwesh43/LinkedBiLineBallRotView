335bba63ded81ab3b1d29664cdcfce9e9ddc5fe2
Anwesh43 <anweshthecool0@gmail.com>
2019-08-03 15:14:20 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbilineballrotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbilineballrotview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbilineballrotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bilineballrotview.BiLineBallRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiLineBallRotView.create(this)
     }
 }
