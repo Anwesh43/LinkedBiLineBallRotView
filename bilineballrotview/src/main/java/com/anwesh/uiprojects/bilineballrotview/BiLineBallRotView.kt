6f722562a00009903543680668a58b272db63d66
Anwesh43 <anweshthecool0@gmail.com>
2019-08-03 13:55:46 +0530

    adding constants and libraries

--- bilineballrotview/src/main/java/com/anwesh/uiprojects/bilineballrotview/BiLineBallRotView.kt
+++ bilineballrotview/src/main/java/com/anwesh/uiprojects/bilineballrotview/BiLineBallRotView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.bilineballrotview
 /**
  * Created by anweshmishra on 03/08/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 2
+val parts : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 90f
+val foreColor : Int = Color.parseColor("#6A1B9A")
+val strokeColor : Int = Color.parseColor("#BDBDBD")
