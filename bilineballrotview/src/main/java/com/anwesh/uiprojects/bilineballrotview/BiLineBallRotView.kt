f4367631b41194aa41feac6124cca6c3dcf48a7a
Anwesh43 <anweshthecool0@gmail.com>
2019-08-03 14:51:17 +0530

    adding BLBRNode de class

--- bilineballrotview/src/main/java/com/anwesh/uiprojects/bilineballrotview/BiLineBallRotView.kt
+++ bilineballrotview/src/main/java/com/anwesh/uiprojects/bilineballrotview/BiLineBallRotView.kt
@@ -133,4 +133,48 @@ class BiLineBallRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLBRNode(var i : Int, val state : State = State()) {
+
+        private var next : BLBRNode? = null
+        private var prev : BLBRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BLBRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLBRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLBRNode {
+            var curr : BLBRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
