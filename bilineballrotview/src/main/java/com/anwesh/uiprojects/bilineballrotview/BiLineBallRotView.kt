31397117f76dbe9ddb7cdf11998092113e200cc2
Anwesh43 <anweshthecool0@gmail.com>
2019-08-03 14:28:51 +0530

    adding Canvas extensions method

--- bilineballrotview/src/main/java/com/anwesh/uiprojects/bilineballrotview/BiLineBallRotView.kt
+++ bilineballrotview/src/main/java/com/anwesh/uiprojects/bilineballrotview/BiLineBallRotView.kt
@@ -21,6 +21,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 90f
 val foreColor : Int = Color.parseColor("#6A1B9A")
 val strokeColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 2.9f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -31,3 +32,39 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawBiLineBall(i : Int, sc : Float, size : Float, paint : Paint) {
+    for (j in 0..(lines - 1)) {
+        save()
+        translate(0f, -size)
+        rotate(45f * (1 - 2 * j) * sc.divideScale(j, lines))
+        drawLine(0f, 0f, 0f, size, paint)
+        drawCircle(0f, size, size / rFactor, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawBiLineBallParts(sc1 : Float, sc2 : Float, size : Float, paint : Paint) {
+    for (j in 0..(parts - 1)) {
+        save()
+        rotate(90f * (1f - 2 * j) * sc2.divideScale(j, parts))
+        drawBiLineBall(j, sc1, size, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawBLBRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawBiLineBallParts(sc1, sc2, size, paint)
+    restore()
+}
